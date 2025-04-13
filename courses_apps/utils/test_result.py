367636182d50c8767e5b8ce838fb18df26671fa1
Suetosha <honey.pancake2017@yandex.ru>
2025-04-13 20:07:53 +0200

    Add function to calculate test results

---
+++ courses_apps/utils/test_result.py
@@ -0,0 +1,76 @@
+from courses_apps.tests.models import Answer
+from courses_apps.utils.compiler import run_code
+
+# Функция подсчёта результатов прохождения теста
+# tasks - Список всех заданий
+# user_answers - Словарь с данными пользователя из заполненной формы
+def calculate_test_result(tasks, user_answers):
+    total_score = 0
+    max_score = 0
+
+    # Проверяем каждую задачу
+    for task in tasks:
+        max_score += task.point
+        # Получаем правильные ответы для задания
+        answers = Answer.objects.filter(task=task)
+
+        # Получаем ответ пользователя для задачи
+        user_answer = user_answers.get(str(task.id))
+        if user_answer is None:
+            continue
+
+        # Если задание с компилятором
+        if task.is_compiler:
+            correct_answer = answers.filter(is_correct=True).first().text
+            # Результат выполнения кода
+            user_result = run_code(user_answer)
+            if user_result == correct_answer:
+                total_score += task.point
+
+        elif task.is_text_input:
+            correct_answer = answers.filter(is_correct=True).first().text
+            if user_answer == correct_answer:
+                total_score += task.point
+
+        # Если задание с множественным выбором
+        elif task.is_multiple_choice:
+            user_correct_answers = 0
+            correct_answers = answers.filter(is_correct=True)
+            total_answers = correct_answers.count()
+
+            for answer in correct_answers:
+                if str(answer.id) in user_answer:
+                    user_correct_answers += 1
+
+            if total_answers > 0:
+                user_correct_count = user_correct_answers / total_answers
+                if user_correct_count > 0.5:
+                    total_score += task.point
+                else:
+                    total_score += 0.5 * task.point
+
+        # Если задание с радио баттоном
+        else:
+            correct_answer = answers.filter(is_correct=True).first()
+            if user_answer == str(correct_answer.id):
+                total_score += task.point
+
+    # Подсчет процента успешности
+    score_percent = int((total_score / max_score) * 100) if max_score > 0 else 0
+
+    # Оценка на основе процента
+    if score_percent <= 50:
+        result = "Не сдал"
+    elif 51 <= score_percent <= 60:
+        result = "Удовлетворительно"
+    elif 61 <= score_percent <= 85:
+        result = "Хорошо"
+    else:
+        result = "Отлично"
+
+    return {
+        "total_score": total_score,  # Набранные баллы
+        "max_score": max_score,  # Максимально возможные баллы
+        "score_percent": score_percent,  # Процент успешности
+        "result": result  # Итог (текстовая оценка)
+    }
